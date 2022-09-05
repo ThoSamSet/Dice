691521942a1c50efc3687ec6e4ab0673f8275737
Finch <puansuanzun@gmail.com>
2022-09-05 22:16:55 +0900

    Hoàn thành bài tập Dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,21 +13,20 @@ class ViewController: UIViewController {
 //    IBOutlet allows me to refenrece a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        Code từ đây
-//        WHO           WHAT    VALUE
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5;
-        
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-    }
 
+//    Roll button
     @IBAction func rollButtonpressed(_ sender: UIButton) {
+        
+        
         print("Button got tapped.")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+            
     }
     
 }
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+//This is a comment
+/** This is a multiline comment**/
+
+print("Hello world!")
+print("Phép tính 2 + 3 có kết quả là: \(2+3)")
+
